32dced08a7f633b73eeb0a3840db23c212211d68
David Aerne <meodai@gmail.com>
2020-07-25 00:02:33 +0200

    reveal effect with html

--- src/index.js
+++ src/index.js
@@ -1,89 +1,27 @@
 console.clear();
 
-import * as PIXI from 'pixi.js';
+const $video = document.querySelector(".header-video source");
 
-const ticker = PIXI.Ticker.shared;
-ticker.autoStart = false;
-ticker.stop();
+const videoSrc = $video.getAttribute('src');
+const videoElements = 3;
 
-const app = new PIXI.Application({
-  transparent: false,
-  width: window.innerWidth,
-  height: window.innerHeight,
+let videoElementsArr = new Array(videoElements).fill('');
+
+const $header = document.querySelector('.video-header');
+
+videoElementsArr = videoElementsArr.map(() => {
+  const $video = document.createElement("video");
+  $video.setAttribute("src", videoSrc);
+  const $wrap = document.createElement("div");
+  $wrap.classList.add("video-header--slice");
+  $wrap.appendChild($video);
+  $header.appendChild($wrap);
+  return $video;
 });
 
-document.body.appendChild(app.view);
-
-var video = document.createElement("video");
-video.preload = "auto";
-video.muted = true;
-video.src = "01-digitaleVermarktung-preview-v006-nosound.mp4";
-video.loop = true;
-
-// create a video texture from a path
-const texture = PIXI.Texture.from(
-  video
-);
-
-const videoPartContainers = [
-  new PIXI.Container(),
-  new PIXI.Container(),
-  new PIXI.Container()
-];
-
-const videoSprites = [];
-
-const partSize = 1 / videoPartContainers.length;
-
-videoPartContainers.forEach((container, i) => {
-
-  const rect = new PIXI.Graphics();
-
-  // create a new Sprite using the video texture (yes it's that easy)
-  const videoSprite = new PIXI.Sprite(texture);
-  videoSprites.push(videoSprite);
-
-  // Stetch the fullscreen
-  videoSprite.width = app.screen.width;
-  videoSprite.height = app.screen.height;
-
-  // Rectangle
-  rect.beginFill(0xffffff);
-  rect.drawRect(
-    partSize * app.screen.width * i,
-    0,
-    partSize * app.screen.width + 1,
-    app.screen.height
-  );
-  rect.endFill();
-
-  container.position.x = app.screen.width * 3;
-  videoSprite.position.x = partSize * app.screen.width * -i;
-
-  /*videoSprite.setTransform(
-    -200 * i
-  );*/
-
-  //container.toLocal(new PIXI.Point(0, 0), videoSprite, videoSprite.position);
-
-  container.addChild(videoSprite);
-  container.mask = rect;
-
-  app.stage.addChild(container);
-});
-
-ticker.start();
-
-const acceleration = 0.025;
-
-setTimeout(() => {
-  ticker.add((time) => {
-    videoPartContainers.forEach((videoSprite, i) => {
-      //videoSprite.position.x -= 5
-      const goal = partSize * app.screen.width * i;
-      const diffx = goal - videoSprite.position.x;
-
-      videoSprite.position.x += diffx * acceleration;
-    });
+document.addEventListener('click', () => {
+  $header.classList.toggle("is-open");
+  videoElementsArr.forEach(element => {
+    element.play();
   });
-}, 1000);
+})

0be442afa78125e644edd5479dcb7a2607004c7a
Joris Noordermeer <joris@noordermeer.ch>
2020-08-10 12:16:17 +0200

    Add config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,39 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended',
+    ],
+    plugins: [
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        semi: ['error', 'always'],
+        quotes: ['error', 'single', { avoidEscape: true }],
+        indent: ['warn', 4, { SwitchCase: 1 }],
+        'comma-dangle': ['error', 'always-multiline'],
+        'vue/comma-dangle': ['error', 'always-multiline'],
+        'vue/script-indent': ['warn', 4, { baseIndent: 1, switchCase: 1 }],
+        'vue/html-indent': ['warn', 4],
+        'vue/max-attributes-per-line': ['error', { singleline: 2 }],
+        'vue/match-component-file-name': ['error', {
+            extensions: ['vue'],
+        }],
+    },
+    overrides: [
+        {
+            files: ['*.vue'],
+            rules: {
+                indent: 'off',
+            },
+        },
+    ],
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -15,11 +15,11 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Raumgleiter',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }

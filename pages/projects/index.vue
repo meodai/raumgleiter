4898705140238ab45a8d3d4d19006d17f8dacaa4
Joris Noordermeer <joris@noordermeer.ch>
2020-08-18 16:53:26 +0200

    List categories

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -10,16 +10,26 @@ export default {
         }
     },
     async asyncData ({ $axios }) {
+
+        const allCategories = collect(await $axios.$get('/categories.json').then(data => data.data))
+        .groupBy('lang').map((cat) => cat.groupBy('group').all()).all();
         // Load all projects when building static site
         const allProjectEntries = collect(await $axios.$get('/projects.json').then(data => data.data))
         .groupBy('lang').all();
-        return { allProjectEntries };
+        return { allProjectEntries, allCategories };
     },
     computed: {
-        projects () {
+        categoriesInCurrentLanguage () {
+            return this.allCategories[this.$i18n.locale];
+        },
+        projectsInCurrentLanguage () {
             // Get localized project entries
             return this.allProjectEntries[this.$i18n.locale];
         },
+        offerCategories () {
+            // We could add the filter query to the url
+            return this.$route.query.offer.split(',');
+        }
     },
 }
 </script>
@@ -27,9 +37,23 @@ export default {
 <template>
     <div>
         Projekt-Übersicht
+        <hr>
+        <h2>Angebote</h2>
         <ul>
             <li
-                v-for="project in projects"
+                v-for="offer in categoriesInCurrentLanguage.offers"
+            >
+                <nuxt-link
+                    :to="localePath({ query: { offer: offer.slug } })"
+                >
+                    {{ offer.title }}
+                </nuxt-link>
+            </li>
+        </ul>
+        <hr>
+        <ul>
+            <li
+                v-for="project in projectsInCurrentLanguage"
                 :key="'project'+project.slug"
             >
                 <nuxt-link

ea10e2da22b24b0ae293d614f407342943b23404
Joris Noordermeer <joris@noordermeer.ch>
2020-08-24 16:45:17 +0200

    Pass locale slugs to i18n

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -2,30 +2,34 @@
 import collect from "collect.js";
 
 export default {
-    nuxtI18n: {
-        paths: {
-            de: '/projekte/:slug',
-            fr: '/projets/:slug',
-            en: '/projects/:slug',
-        }
-    },
-    async asyncData ({ $axios, params }) {
-        const projectEntry = collect(await $axios.$get(`/projects/${params.slug}.json`).then(data => data.data))
-        .groupBy('lang').all();
-        return { projectEntry };
-    },
-    computed: {
-        projectInCurrentLocale () {
-            // Get localized project entry
-            return collect(this.projectEntry[this.$i18n.locale]).first();
-        },
+  nuxtI18n: {
+    paths: {
+      de: '/projekte/:slug',
+      fr: '/projets/:slug',
+      en: '/projects/:slug',
     }
+  },
+  async asyncData({$axios, params, store}) {
+    let projectEntry = collect(await $axios.$get(`/projects/${params.slug}.json`).then(data => data.data))
+    .keyBy('lang');
+    await store.dispatch('i18n/setRouteParams', projectEntry.first().locale_slugs);
+
+    return { projectEntry: projectEntry.all() };
+  },
+  computed: {
+    projectInCurrentLocale()
+    {
+      return this.projectEntry[this.$i18n.locale]
+        // Fallback for dev environment
+        || this.projectEntry[Object.keys(this.projectEntry)[0]];
+    },
+  },
 }
 </script>
 
 <template>
-    <div>
-        <nuxt-link :to="localePath('projects')">Zurück zu Projekten</nuxt-link>
-        <h1>{{ projectInCurrentLocale.title }}</h1>
-    </div>
+  <div>
+    <nuxt-link :to="localePath('projects')">Zurück zu Projekten</nuxt-link>
+    <h1>{{ projectInCurrentLocale.title }}</h1>
+  </div>
 </template>

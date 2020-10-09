8f9dbe2dbfdaa0b09b1b6c6084ebd6158ccd43c5
Joris Noordermeer <joris@noordermeer.ch>
2020-10-09 15:15:28 +0200

    lint

--- pages/design-inventory.vue
+++ pages/design-inventory.vue
@@ -2,12 +2,12 @@
   export default {
     data () {
       return {
-        sizes: ['mouse', 'rat', 'cat', 'dog', 'pony', 'horse'],
-        types: ['default', 'title', 'title--page', 'title--hero', 'lead'],
+        sizes: ['mouse', 'rat', 'cat', 'dog', 'pony', 'horse', ],
+        types: ['default', 'title', 'title--page', 'title--hero', 'lead', ],
       };
     },
     methods: {
-      exampleImage(id, title, width = 1600, height = 900) {
+      exampleImage (id, title, width = 1600, height = 900,) {
         return {
           width,
           height,
@@ -15,9 +15,9 @@
           title,
           alt: title,
           src: `https://picsum.photos/id/${id}/${width}/${height}`,
-          srcset: `https://picsum.photos/id/${id}/${width*0.25}/${height*0.25} ${width*0.25}w, https://picsum.photos/id/${id}/${width*0.5}/${height*0.5} ${width*0.5}w, https://picsum.photos/id/${id}/${width*0.75}/${height*0.75} ${width*0.75}w, https://picsum.photos/id/${id}/${width}/${height} ${width}w`,
-        }
-      }
+          srcset: `https://picsum.photos/id/${id}/${width * 0.25}/${height * 0.25} ${width * 0.25}w, https://picsum.photos/id/${id}/${width * 0.5}/${height * 0.5} ${width * 0.5}w, https://picsum.photos/id/${id}/${width * 0.75}/${height * 0.75} ${width * 0.75}w, https://picsum.photos/id/${id}/${width}/${height} ${width}w`,
+        };
+      },
     },
   };
 </script>
@@ -25,18 +25,37 @@
 <template>
   <div>
     <div class="l-design-width">
-      <h1 class="t-title t-title--hero">Design Inventory</h1>
-      <h2 class="t-title t-title--page t-title--inventory">Sizes</h2>
+      <h1 class="t-title t-title--hero">
+        Design Inventory
+      </h1>
+      <h2 class="t-title t-title--page t-title--inventory">
+        Sizes
+      </h2>
       <div class="sizes">
-        <div class="size" v-for="(size, i) in sizes" :key="i" v-bind:style="{'--size': `var(--size-${size})`}"><span>{{size}}</span></div>
+        <div
+          v-for="(size, i) in sizes"
+          :key="i"
+          class="size"
+          :style="{'--size': `var(--size-${size})`,}"
+        >
+          <span>{{ size }}</span>
+        </div>
       </div>
 
-      <h2 class="t-title t-title--page t-title--inventory">Typography</h2>
+      <h2 class="t-title t-title--page t-title--inventory">
+        Typography
+      </h2>
       <div class="typo">
-        <span v-for="(typo, i) in types" :key="i" v-bind:class="`t-${typo}`">t-{{typo}}</span>
+        <span
+          v-for="(typo, i) in types"
+          :key="i"
+          :class="`t-${typo}`"
+        >t-{{ typo }}</span>
       </div>
 
-      <h2 class="t-title t-title--page t-title--inventory">Grid</h2>
+      <h2 class="t-title t-title--page t-title--inventory">
+        Grid
+      </h2>
       <div class="l-grid">
         <div class="debug l-grid__cell l-grid__cell--1/2">
           <h3>Width 1/2</h3>
@@ -95,19 +114,23 @@
       </div>
     </div>
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Navigation</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Navigation
+    </h2>
     <div style="background: #f04e45; height: 50vh; overflow: hidden;">
       <navigation />
     </div>
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Quote</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Quote
+    </h2>
     <Quote
       :fields="{
         body: 'Zusammen mit Raumgleiter haben wir anfänglich Neuland betreten. Mittlerweile ist unser ganzes Facility Management täglich auf dem Konfigurator unterwegs. Ein Gewinn an Zeit und ein Gewinn für unser Unternehmen.',
         author: 'Svetlana Kuster, ZKB',
         background: {
           type: 'image',
-          image: exampleImage(1038, 'kitten'),
+          image: exampleImage(1038, 'kitten',),
         },
       }"
     />
@@ -118,65 +141,20 @@
         body: 'Wer glaubt etwas zu sein, hat aufgehört es zu werden',
         background: {
           type: 'image',
-          image: exampleImage(1038, 'kitten'),
+          image: exampleImage(1038, 'kitten',),
         },
       }"
     />
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Image-Block</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Image-Block
+    </h2>
     <ImageBlock
       :fields="{
         header: 'Vision.',
         body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
         images:[
-          exampleImage(1038, 'kitten'),
-        ]
-      }"
-    />
-
-    <ImageBlock
-      :fields="{
-        header: 'Evaluationsphase.',
-        body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
-        imagePosition: 'right',
-        images:[
-          exampleImage(1038, 'kitten'),
-        ],
-      }"
-    />
-
-    <ImageBlock
-      :fields="{
-        header: '360° Panorama.',
-        body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
-        imagePosition: 'top',
-        images:[
-          exampleImage(1019, 'sky'),
-        ],
-      }"
-    />
-
-    <ImageBlock
-      :fields="{
-        header: 'Kollaborative Plattform.',
-        body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
-        imagePosition: 'background',
-        images:[
-        exampleImage(1021, 'people'),
-        ],
-      }"
-    />
-
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Image-Block Gallery</h2>
-    <ImageBlock
-      :fields="{
-        header: 'Vision.',
-        body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
-        images:[
-          exampleImage(1042, 'people', 700, 438),
-          exampleImage(1047, 'people', 700, 438),
-          exampleImage(1052, 'people', 700, 438),
-          exampleImage(153, 'people', 700, 438),
+          exampleImage(1038, 'kitten',),
         ],
       }"
     />
@@ -187,10 +165,7 @@
         body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
         imagePosition: 'right',
         images:[
-          exampleImage(1042, 'people', 700, 438),
-          exampleImage(1047, 'people', 700, 438),
-          exampleImage(1052, 'people', 700, 438),
-          exampleImage(153, 'people', 700, 438),
+          exampleImage(1038, 'kitten',),
         ],
       }"
     />
@@ -201,8 +176,7 @@
         body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
         imagePosition: 'top',
         images:[
-          exampleImage(1053, 'people', 1250, 500),
-          exampleImage(1060, 'people', 1250, 500),
+          exampleImage(1019, 'sky',),
         ],
       }"
     />
@@ -213,18 +187,73 @@
         body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
         imagePosition: 'background',
         images:[
-          exampleImage(1021, 'people', 1440, 1000),
-          exampleImage(1022, 'people', 1440, 1000),
-          exampleImage(1044, 'people', 1440, 1000),
-          exampleImage(1064, 'people', 1440, 1000),
-          exampleImage(999, 'people', 1440, 1000),
+          exampleImage(1021, 'people',),
         ],
       }"
     />
 
-<!--    <h2 class="l-design-width t-title t-title&#45;&#45;page t-title&#45;&#45;inventory">Image-Block with Video</h2>-->
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Image-Block Gallery
+    </h2>
+    <ImageBlock
+      :fields="{
+        header: 'Vision.',
+        body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
+        images:[
+          exampleImage(1042, 'people', 700, 438,),
+          exampleImage(1047, 'people', 700, 438,),
+          exampleImage(1052, 'people', 700, 438,),
+          exampleImage(153, 'people', 700, 438,),
+        ],
+      }"
+    />
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Image-Block with Iframe</h2>
+    <ImageBlock
+      :fields="{
+        header: 'Evaluationsphase.',
+        body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
+        imagePosition: 'right',
+        images:[
+          exampleImage(1042, 'people', 700, 438,),
+          exampleImage(1047, 'people', 700, 438,),
+          exampleImage(1052, 'people', 700, 438,),
+          exampleImage(153, 'people', 700, 438,),
+        ],
+      }"
+    />
+
+    <ImageBlock
+      :fields="{
+        header: '360° Panorama.',
+        body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
+        imagePosition: 'top',
+        images:[
+          exampleImage(1053, 'people', 1250, 500,),
+          exampleImage(1060, 'people', 1250, 500,),
+        ],
+      }"
+    />
+
+    <ImageBlock
+      :fields="{
+        header: 'Kollaborative Plattform.',
+        body: 'Im Bereich virtueller Realitäten können wir alles. Fast alles. Denn seit 20 Jahren wir sind der Schweizer Pionier für zukunftsweisende Visualisierungen, der Ihnen das bietet, worauf es ankommt: Ein breites Angebot. Und eine überragende Qualität.',
+        imagePosition: 'background',
+        images:[
+          exampleImage(1021, 'people', 1440, 1000,),
+          exampleImage(1022, 'people', 1440, 1000,),
+          exampleImage(1044, 'people', 1440, 1000,),
+          exampleImage(1064, 'people', 1440, 1000,),
+          exampleImage(999, 'people', 1440, 1000,),
+        ],
+      }"
+    />
+
+    <!--    <h2 class="l-design-width t-title t-title&#45;&#45;page t-title&#45;&#45;inventory">Image-Block with Video</h2>-->
+
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Image-Block with Iframe
+    </h2>
 
     <ImageBlock
       :fields="{
@@ -235,14 +264,17 @@
       }"
     />
 
-
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Inverted Section</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Inverted Section
+    </h2>
     <div class="l-design-width c-design c-design--inverted">
       <h1>Some Inverted Text</h1>
       <p>Lorem Ipsum doror sit amet</p>
     </div>
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">CTA</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      CTA
+    </h2>
 
     <CallToAction
       class="c-design c-design--inverted"
@@ -257,7 +289,6 @@
       }"
     />
 
-
     <CallToAction
       :fields="{
         icon: 'heart',
@@ -270,7 +301,9 @@
       }"
     />
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Intro Section</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Intro Section
+    </h2>
     <intro
       :fields="{
         header: 'Virtuelle Vermarktung.',
@@ -279,7 +312,9 @@
       }"
     />
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Intro Section with List</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Intro Section with List
+    </h2>
 
     <intro
       :fields="{
@@ -296,11 +331,13 @@
               'Virtuelle Lösungen für den Wiederverkauf und -Vermietung',
             ],
           },
-        ]
+        ],
       }"
     />
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Intro Section with List and CTA</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Intro Section with List and CTA
+    </h2>
 
     <intro
       :fields="{
@@ -317,7 +354,7 @@
               'Virtuelle Lösungen für den Wiederverkauf und -Vermietung',
             ],
           },
-        ]
+        ],
       }"
     >
       <CallToAction
@@ -333,50 +370,60 @@
       />
     </intro>
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Related Slider</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Related Slider
+    </h2>
     <Related
       title="Weitere Welten"
       :entries="[
-          {image: exampleImage(1042, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(1047, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(1052, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(153, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(153, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(1042, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(1047, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(1052, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-        ]"
+        {image: exampleImage(1042, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(1047, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(1052, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(153, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(153, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(1042, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(1047, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(1052, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+      ]"
     />
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Related Slider with 1 entry</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Related Slider with 1 entry
+    </h2>
     <Related
       title="Weitere Welten"
       :entries="[
-          {image: exampleImage(1042, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-        ]"
+        {image: exampleImage(1042, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+      ]"
     />
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Related Slider with 2 entries</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Related Slider with 2 entries
+    </h2>
     <Related
       title="Weitere Welten"
       :entries="[
-          {image: exampleImage(1042, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(1047, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-        ]"
+        {image: exampleImage(1042, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(1047, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+      ]"
     />
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Related Slider with 3 entries</h2>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Related Slider with 3 entries
+    </h2>
     <Related
       title="Weitere Welten"
       :entries="[
-          {image: exampleImage(1042, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(1047, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-          {image: exampleImage(1048, 'people', 700, 438), slug: 'project-1', title: 'Projektname'},
-        ]"
+        {image: exampleImage(1042, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(1047, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+        {image: exampleImage(1048, 'people', 700, 438,), slug: 'project-1', title: 'Projektname',},
+      ]"
     />
 
-    <h2 class="l-design-width t-title t-title--page t-title--inventory">Page Footer</h2>
-    <PageFooter></PageFooter>
+    <h2 class="l-design-width t-title t-title--page t-title--inventory">
+      Page Footer
+    </h2>
+    <PageFooter />
   </div>
 </template>
 

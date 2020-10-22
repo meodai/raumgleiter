cd934741443566c4c7d2d0b72b4a7748d39d6d87
David Aerne <meodai@gmail.com>
2020-10-22 12:34:57 +0200

    feat(intro): transitions on lists

--- components/Intro.vue
+++ components/Intro.vue
@@ -28,6 +28,7 @@
     },
     data () {
       return {
+        listIntersects: false,
       };
     },
     mounted () {
@@ -49,6 +50,9 @@
         entries.forEach((entry) => {
           this.$nuxt.$emit('intro-intersect', entry.isIntersecting);
         });
+      },
+      listVisibilityChanged (isVisible, entry) {
+        this.listIntersects = isVisible;
       },
     },
   };
@@ -74,12 +78,16 @@
     <aside
       v-for="(offers, index) in fields.offers"
       :key="'offers'+index"
+      v-observe-visibility="{
+        callback: listVisibilityChanged,
+        once: true,
+      }"
       class="intro__list-wrap"
     >
       <h3 class="intro__list-title t-title">
         {{ offers.header }}
       </h3>
-      <ul class="intro__list">
+      <ul class="intro__list" :class="{'intro__list--visibile': listIntersects}">
         <li
           v-for="listItem in offers.services"
           :key="listItem"
@@ -148,6 +156,20 @@
     margin-left: 1em;
     margin-top: 1ex;
     list-style: disc;
+    opacity: 0;
+    transform: translateY(2rem);
+    transition: 100ms opacity linear, 444ms transform cubic-bezier(0.3, 0.7, 0, 1);
+
+    @for $i from 1 through 20 {
+      &:nth-child(#{$i}) {
+        transition-delay: 500ms + $i * 75ms;
+      }
+    }
+
+    .intro__list--visibile & {
+      opacity: 1;
+      transform: scale(1) translateY(0);
+    }
   }
 
   .intro__cta {

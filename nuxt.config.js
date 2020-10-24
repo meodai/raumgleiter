7354e07db56ac48de891400f412ff535fcb74ad1
Joris Noordermeer <joris@noordermeer.ch>
2020-10-24 16:21:15 +0200

    try not to expose api url to browser

--- nuxt.config.js
+++ nuxt.config.js
@@ -169,10 +169,10 @@ export default {
     livePreview: process.env.LIVE_PREVIEW === 'true',
     devMode: process.env.DEV_MODE === 'true',
     http: {
-      browserBaseURL: process.env.API_URL || 'https://api.raumgleiter.noo.work',
-      // process.env.LIVE_PREVIEW === 'true' || process.env.DEV_MODE === 'true'
-      //   ? process.env.API_URL || 'https://api.raumgleiter.noo.work'
-      //   : '',
+      // browserBaseURL: process.env.API_URL || 'https://api.raumgleiter.noo.work',
+      browserBaseURL: process.env.LIVE_PREVIEW === 'true' || process.env.DEV_MODE === 'true'
+        ? process.env.API_URL || 'https://api.raumgleiter.noo.work'
+        : '',
     },
   },
 };

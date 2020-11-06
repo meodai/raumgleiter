9678146bc4a519c0d1cefd417e5a0e0a50cc6f7c
Joris Noordermeer <joris@noordermeer.ch>
2020-11-06 14:40:13 +0100

    Set scroll timeout and log craft requests

--- plugins/scroll-to-top.js
+++ plugins/scroll-to-top.js
@@ -1,3 +1,13 @@
 import Vue from 'vue';
-Vue.prototype.$scrollToTop = () => window.scrollTo(0, 0);
-Vue.prototype.$scrollToBottom = () => window.scrollTo(0, document.body.scrollHeight);
+Vue.prototype.$scrollToTop = () => {
+  window.scrollTo(0, 0);
+  setTimeout(() => {
+    window.scrollTo(0, 0);
+  }, 200);
+};
+Vue.prototype.$scrollToBottom = () => {
+  window.scrollTo(0, document.body.scrollHeight);
+  setTimeout(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  }, 200);
+};
